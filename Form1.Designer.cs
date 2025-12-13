e067e533845dd90b461e41932106dd5ed49db991
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-12-13 23:13:53 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace SpillBucketA3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.BtnColor = new System.Windows.Forms.Button();
+            this.BtnPencil = new System.Windows.Forms.Button();
+            this.BtnEllipse = new System.Windows.Forms.Button();
+            this.BtnLine = new System.Windows.Forms.Button();
+            this.BtnRectangle = new System.Windows.Forms.Button();
+            this.BtnEraser = new System.Windows.Forms.Button();
+            this.BtnPaint = new System.Windows.Forms.Button();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.Pic = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.BtnColor);
+            this.flowLayoutPanel1.Controls.Add(this.BtnPencil);
+            this.flowLayoutPanel1.Controls.Add(this.BtnEllipse);
+            this.flowLayoutPanel1.Controls.Add(this.BtnLine);
+            this.flowLayoutPanel1.Controls.Add(this.BtnRectangle);
+            this.flowLayoutPanel1.Controls.Add(this.BtnEraser);
+            this.flowLayoutPanel1.Controls.Add(this.BtnPaint);
+            this.flowLayoutPanel1.Controls.Add(this.BtnClear);
+            this.flowLayoutPanel1.Controls.Add(this.BtnSave);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(231, 626);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // BtnColor
+            // 
+            this.BtnColor.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnColor.BackgroundImage")));
+            this.BtnColor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnColor.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnColor.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Cyan;
+            this.BtnColor.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnColor.Location = new System.Drawing.Point(3, 3);
+            this.BtnColor.Name = "BtnColor";
+            this.BtnColor.Size = new System.Drawing.Size(74, 59);
+            this.BtnColor.TabIndex = 0;
+            this.BtnColor.UseVisualStyleBackColor = true;
+            // 
+            // BtnPencil
+            // 
+            this.BtnPencil.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnPencil.BackgroundImage")));
+            this.BtnPencil.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnPencil.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnPencil.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Cyan;
+            this.BtnPencil.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnPencil.Location = new System.Drawing.Point(83, 3);
+            this.BtnPencil.Name = "BtnPencil";
+            this.BtnPencil.Size = new System.Drawing.Size(65, 59);
+            this.BtnPencil.TabIndex = 1;
+            this.BtnPencil.UseVisualStyleBackColor = true;
+            this.BtnPencil.Click += new System.EventHandler(this.BtnPencil_Click);
+            // 
+            // BtnEllipse
+            // 
+            this.BtnEllipse.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnEllipse.BackgroundImage")));
+            this.BtnEllipse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnEllipse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnEllipse.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Cyan;
+            this.BtnEllipse.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnEllipse.Location = new System.Drawing.Point(154, 3);
+            this.BtnEllipse.Name = "BtnEllipse";
+            this.BtnEllipse.Size = new System.Drawing.Size(71, 59);
+            this.BtnEllipse.TabIndex = 2;
+            this.BtnEllipse.UseVisualStyleBackColor = true;
+            // 
+            // BtnLine
+            // 
+            this.BtnLine.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnLine.BackgroundImage")));
+            this.BtnLine.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnLine.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnLine.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Cyan;
+            this.BtnLine.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnLine.Location = new System.Drawing.Point(3, 68);
+            this.BtnLine.Name = "BtnLine";
+            this.BtnLine.Size = new System.Drawing.Size(74, 59);
+            this.BtnLine.TabIndex = 3;
+            this.BtnLine.UseVisualStyleBackColor = true;
+            // 
+            // BtnRectangle
+            // 
+            this.BtnRectangle.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnRectangle.BackgroundImage")));
+            this.BtnRectangle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnRectangle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnRectangle.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Cyan;
+            this.BtnRectangle.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnRectangle.Location = new System.Drawing.Point(83, 68);
+            this.BtnRectangle.Name = "BtnRectangle";
+            this.BtnRectangle.Size = new System.Drawing.Size(65, 59);
+            this.BtnRectangle.TabIndex = 4;
+            this.BtnRectangle.UseVisualStyleBackColor = true;
+            // 
+            // BtnEraser
+            // 
+            this.BtnEraser.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnEraser.BackgroundImage")));
+            this.BtnEraser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnEraser.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnEraser.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Cyan;
+            this.BtnEraser.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnEraser.Location = new System.Drawing.Point(154, 68);
+            this.BtnEraser.Name = "BtnEraser";
+            this.BtnEraser.Size = new System.Drawing.Size(71, 59);
+            this.BtnEraser.TabIndex = 5;
+            this.BtnEraser.UseVisualStyleBackColor = true;
+            this.BtnEraser.Click += new System.EventHandler(this.BtnEraser_Click);
+            // 
+            // BtnPaint
+            // 
+            this.BtnPaint.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnPaint.BackgroundImage")));
+            this.BtnPaint.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnPaint.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnPaint.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Cyan;
+            this.BtnPaint.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnPaint.Location = new System.Drawing.Point(3, 133);
+            this.BtnPaint.Name = "BtnPaint";
+            this.BtnPaint.Size = new System.Drawing.Size(74, 63);
+            this.BtnPaint.TabIndex = 6;
+            this.BtnPaint.UseVisualStyleBackColor = true;
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnSave.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Cyan;
+            this.BtnSave.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnSave.Location = new System.Drawing.Point(154, 133);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(71, 63);
+            this.BtnSave.TabIndex = 7;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnClear.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Cyan;
+            this.BtnClear.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnClear.Location = new System.Drawing.Point(83, 133);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(65, 63);
+            this.BtnClear.TabIndex = 9;
+            this.BtnClear.Text = "Clear";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            // 
+            // Pic
+            // 
+            this.Pic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Pic.Location = new System.Drawing.Point(231, 0);
+            this.Pic.Name = "Pic";
+            this.Pic.Size = new System.Drawing.Size(1079, 626);
+            this.Pic.TabIndex = 1;
+            this.Pic.TabStop = false;
+            this.Pic.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Pic_MouseDown);
+            this.Pic.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Pic_MouseMove);
+            this.Pic.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Pic_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1310, 626);
+            this.Controls.Add(this.Pic);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SpillBucket";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button BtnColor;
+        private System.Windows.Forms.Button BtnPencil;
+        private System.Windows.Forms.Button BtnEllipse;
+        private System.Windows.Forms.Button BtnLine;
+        private System.Windows.Forms.Button BtnRectangle;
+        private System.Windows.Forms.Button BtnEraser;
+        private System.Windows.Forms.Button BtnPaint;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.PictureBox Pic;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpillBucketA3
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bm;
+        Graphics g;
+        bool paint = false;
+        Point px, py;
+        Pen p = new Pen(Color.Black, 1);
+        Pen Eraser = new Pen(Color.White, 10);
+        int index;
+        public Form1()
+        {
+            InitializeComponent();
+            bm = new Bitmap(Pic.Width, Pic.Height);
+            g = Graphics.FromImage(bm);
+            g.Clear(Color.White);
+            Pic.Image = bm;
+        }
+
+        private void Pic_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+            py = e.Location; 
+        }
+
+        private void Pic_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (paint)
+            {
+                if(index == 1)
+                {
+                    px = e.Location;
+                    g.DrawLine(p, px, py);
+                    py = px;
+                }
+                if(index == 2)
+                {
+                    px = e.Location;
+                    g.DrawLine(Eraser, px, py);
+                    py = px;
+                }
+            }
+            Pic.Refresh();
+        }
+
+        private void Pic_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint = false;
+        }
+
+        private void BtnEraser_Click(object sender, EventArgs e)
+        {
+            index = 2;
+        }
+
+        private void BtnPencil_Click(object sender, EventArgs e)
+        {
+            index = 1;
+        }
+    }
+}
